543c76467f207b9c8209f8eea83969afb9d9ab8d
chiaraiurato <92391371+chiaraiurato@users.noreply.github.com>
2025-05-16 14:48:45 +0200

    Update nifi conf
    - Now nifi works and put the parquet file into hdfs
    - TODO: create a script for master to automatize the flow

--- docker/hadoop/start.sh
+++ docker/hadoop/start.sh
@@ -4,13 +4,16 @@ hdfs namenode -format
 
 WORKERS_FILE=$HADOOP_HOME/etc/hadoop/workers
 
-if [ "$1" = "--format" ]; then
-    # Before doing anything, just mantain slave1 in the workers file, delete the rest
-    head -n 1 $WORKERS_FILE > tmpfile && mv tmpfile $WORKERS_FILE
-    # Format
-    hdfs namenode -format
-fi
+# if [ "$1" = "--format" ]; then
+#     # Before doing anything, just mantain slave1 in the workers file, delete the rest
+#     head -n 1 $WORKERS_FILE > tmpfile && mv tmpfile $WORKERS_FILE
+#     # Format
+#     hdfs namenode -format
+# fi
 
 $HADOOP_HOME/sbin/start-dfs.sh
 
 hdfs dfs -mkdir /nifi
+hdfs dfs -chmod 777 /nifi
+
+hadoop fs -copyToLocal /nifi/merged.parquet /tmp/merged.parquet
